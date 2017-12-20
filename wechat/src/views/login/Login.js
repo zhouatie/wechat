dd3f6d55cb8beff72a11feb6f4aee3cfb7d449e2
zhouatie <1114349984@qq.com>
2017-12-21 01:20:00 +0800

    201712210119

--- wechat/src/views/login/Login.js
+++ wechat/src/views/login/Login.js
@@ -7,9 +7,10 @@ class Login extends Component {
       <div id="login">
         <span className=" close iconfont icon-close"></span>
         <div className="login_inputs">
-            <div className="input_wrap"><input type="text" placeholder="请输入账号"/></div>
-           <div className="input_wrap"><input type="password" placeholder="请输入密码"/></div>
+            <div className="input_wrap"><input id="username" type="text" placeholder="请输入账号"/></div>
+           <div className="input_wrap"><input id="password" type="password" placeholder="请输入密码"/></div>
         </div>
+        <div className="login_btn disabled">登录</div>
       </div>
     );
   }
