2bfc44f5ba6850fe328720362045ab4bb9e4d339
zhouatie <1114349984@qq.com>
2018-01-01 21:21:28 +0800

    2018-01-01 21:21

--- server/app.js
+++ server/app.js
@@ -8,7 +8,23 @@ app.use(bodyParser.json())
 
 var User = require("./mongo/models/login.js");
 
+app.use(express.static("../wechat/build/"));
+
 let user = {};
+
+// 允许跨域
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); /让options请求快速返回/
+  }
+  else {
+    next();
+  }
+});
 // 注册
 app.post("/register", function (req, res) {
 
@@ -124,46 +140,21 @@ var onlineCount = 0;
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+        io.emit('message', '还你消息');
     //监听新用户加入
     socket.on('login', function (obj) {
-        //将新加入用户的唯一标识当作socket的名称，后面退出的时候会用到
-        socket.name = obj.userid;
-
-        //检查在线列表，如果不在里面就加入
-        if (!onlineUsers.hasOwnProperty(obj.userid)) {
-            onlineUsers[obj.userid] = obj.username;
-            //在线人数+1
-            onlineCount++;
-        }
-
-        //向所有客户端广播用户加入
-        io.emit('login', { onlineUsers: onlineUsers, onlineCount: onlineCount, user: obj });
-        console.log(obj.username + '加入了聊天室');
+        console.log(obj,'server obj');
+        io.emit('message', '还你消息');
     });
 
     //监听用户退出
     socket.on('disconnect', function () {
-        //将退出的用户从在线列表中删除
-        if (onlineUsers.hasOwnProperty(socket.name)) {
-            //退出用户的信息
-            var obj = { userid: socket.name, username: onlineUsers[socket.name] };
-
-            //删除
-            delete onlineUsers[socket.name];
-            //在线人数-1
-            onlineCount--;
-
-            //向所有客户端广播用户退出
-            io.emit('logout', { onlineUsers: onlineUsers, onlineCount: onlineCount, user: obj });
-            console.log(obj.username + '退出了聊天室');
-        }
+        
     });
 
     //监听用户发布聊天内容
     socket.on('message', function (obj) {
-        //向所有客户端广播发布的消息
-        io.emit('message', obj);
-        console.log(obj.username + '说：' + obj.content);
+        
     });
 });
 
--- wechat/src/views/chat/Chat.js
+++ wechat/src/views/chat/Chat.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react'
 import './chat.css'
 import { connect } from 'react-redux'
 import Header from '../header/Header.js'
-
+// const socket = require('socket.io-client')('0.0.0.0:4000');
+// import * as io from 'socket.io-client'
 
 
 class Chat extends Component {
 
 
     componentDidMount() {
-
+        this.loadSocket();
+    }
+    loadSocket() {
+        var _this = this;
+        // _this.socket = io.connect('http://localhost:4000', {
+        //     path: '/',
+        //     transports: ['websocket', 'polling']
+        // });
+        // this.socket.on('message',function(data){
+        //     console.log(data,'message data')
+        // })
+        // // this.socket.on('login','jinqule')
+        // this.socket.on("message",function(data){
+        //     console.log(data,'socket data')
+        // })
+        
     }
     onSend = () => {
-        console.log(this.props.userData,'data')
+        console.log(this.props.userData, 'data')
         let message_wrap = document.getElementById("message-wrap");
         let div = document.createElement("div");
         div.className = "self_message message";
-        div.innerHTML = '<div class="message-logo-wrap"><img src="'+this.props.userData.logo+'"/></div><div class="message-info-wrap">' + this.refs.textarea.value + '</div>';
+        div.innerHTML = '<div class="message-logo-wrap"><img src="' + this.props.userData.logo + '"/></div><div class="message-info-wrap">' + this.refs.textarea.value + '</div>';
         message_wrap.appendChild(div);
         this.refs.textarea.value = "";
     }
